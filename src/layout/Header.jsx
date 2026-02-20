9014c4a0db0767df338b047072deba90b5b5d823
XWorkUser <xwork.user1@gmail.com>
2026-02-20 07:31:36 +0400

    added scroll function and updated styles

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,30 @@
 import { NavLink } from "react-router-dom";
 import useGlobalFetch from "../components/useGlobalFetch/useGlobalFetch";
+import { useEffect, useState } from "react";
 
 function Header(params) {
   const { data } = useGlobalFetch();
+  const [scroll, setScroll] = useState(false);
+  const SCROLL_Y = 200;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > SCROLL_Y) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-      <header>
-        <div className="header-block">
+      <header
+        className={`${scroll ? "header-scroll header-scroll-block" : ""}`}
+      >
+        <div className={`header-block ${scroll ? "header-scroll-block" : ""}`}>
           {data?.header &&
             data?.header?.map((item, index) => {
               return (
@@ -15,7 +33,6 @@ function Header(params) {
                 </NavLink>
               );
             })}
-            
         </div>
       </header>
     </>
